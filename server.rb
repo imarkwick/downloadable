c83cba4ff5adf9459099d0f751bc07080b883951
imarkwick <izzymarkwick@gmail.com>
2015-04-10 23:51:59 +0100

    accessing a user stream

--- server.rb
+++ server.rb
@@ -20,17 +20,22 @@ get '/' do
 
 	@downloadable = downloadable_urls
 
+	@stream = @client.get('/me/activities/tracks/affiliated', :limit => 1)
+
+	puts @stream.has_key?("collection")
+	puts '-----------------'
+	puts @stream.collection[0].has_key?("title")
+
 	test_track_url = 'http://soundcloud.com/joehertz/tears'
 	@embed_info = @client.get('/oembed', :url => test_track_url)
-
-	puts @downloadable_tracks.count
-	puts '------------------------'
-
 	# puts @embed_info['html']	
 	# puts @embed_info.is_a?(Hash)
 
 	erb :index
 end
+
+# URL for Joe's soundcloud stream:
+# https://api.soundcloud.com/me/activities/tracks/affiliated?limit=100&oauth_token=719a768af3a4fe513bfac11c4c81e408
 
 get '/downloadable' do
 	# send_file 'index.html'
